332f5efc3e8593fe49f270639917074011d71260
Syed Saqib Ali <Saqibsyed1014@gmail.com>
2025-06-02 15:39:12 +0500

    build fix

--- src/app/instructor/create-new-course/components/MultiStepFormHeader.tsx
+++ src/app/instructor/create-new-course/components/MultiStepFormHeader.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import {Button} from "@/components/ui/button";
 
-const MultiStepFormHeader = ({ title, formStepIndex }) => {
+const MultiStepFormHeader = ({ title, formStepIndex } : { title: string; formStepIndex?: number }) => {
     return (
         <div className="border-b py-6 px-10 flex justify-between items-center">
             <h4>{title}</h4>
